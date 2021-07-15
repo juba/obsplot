765bc5ec738fbf5945707d50d16b6413efbda330
Julien <julien@nozav.org>
2021-07-15 17:06:59 +0200

    Move is_css_color()

--- R/utils.R
+++ R/utils.R
@@ -2,17 +2,6 @@ is_categorical <- function(v) {
     is.character(v) || is.factor(v)
 }
 
-is_css_color <- function(str) {
-    str <- tolower(str)
-    css_colors <- gsub("\\d", "", grDevices::colors())
-    css_colors <- unique(css_colors)
-    css_colors <- c(css_colors, "none", "transparent", "currentcolor")
-    if (str %in% css_colors) return(TRUE)
-    is_hex_code <- grepl("^#[0-9a-f]{6}$", str) || grepl("^#[0-9a-f]{3}$", str)
-    return(is_hex_code)
-}
-
-
 #' Convert a Date or POSIXt object to a JS Date format
 #'
 #' @param date object to be converted
@@ -47,5 +36,3 @@ add_metadata <- function(data) {
   }
   return(list(data = data, dates = NULL))
 }
-
-
