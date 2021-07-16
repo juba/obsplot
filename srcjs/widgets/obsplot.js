87cb34eaf8c220fdf878abbe26a7f3dc73fbf39e
Julien <julien@nozav.org>
2021-07-16 15:25:54 +0200

    Manually add d3 and Plot objects to window

--- srcjs/widgets/obsplot.js
+++ srcjs/widgets/obsplot.js
@@ -1,6 +1,11 @@
 import { Obsplot } from "../modules/plot";
+import * as d3 from "d3";
+import * as Plot from "@observablehq/plot";
 import 'widgets';
 import '../css/plot.css';
+
+window.d3 = d3;
+window.Plot = Plot;
 
 HTMLWidgets.widget({
 
