87119822935799345a8c790fa7ce6bebc26ec268
Julien <julien@nozav.org>
2021-08-27 12:05:50 +0200

    Fix width and height tests

--- tests/testthat/test-opts.R
+++ tests/testthat/test-opts.R
@@ -2,7 +2,7 @@
 
 test_that("width and height in opts are ignored", {
     expect_warning(g <- obsplot() |> opts(width = 200, grid = TRUE))
-    expect_equal(names(g$x$opts), "grid")
+    expect_equal(g$x$opts$width, "auto")
     expect_warning(g <- obsplot() |> opts(height = 400, grid = TRUE))
-    expect_equal(names(g$x$opts), "grid")
+    expect_equal(g$x$opts$height, "auto")
 })
