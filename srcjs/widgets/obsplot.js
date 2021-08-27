36e0f17c1428246e1e144d31289b9e8be733629b
Julien <julien@nozav.org>
2021-08-27 16:06:36 +0200

    Make stdlib svg function available in JS() calls

--- srcjs/widgets/obsplot.js
+++ srcjs/widgets/obsplot.js
@@ -2,12 +2,17 @@ import 'core-js/features/object/from-entries';
 import { Obsplot } from "../modules/plot";
 import * as d3 from "d3";
 import * as Plot from "@observablehq/plot";
+import { Library } from "@observablehq/stdlib";
 import 'widgets';
 import '../css/plot.css';
 
 // Make libraries available to be used in JS()
 window.d3 = d3;
 window.Plot = Plot;
+// Observable standard library
+const library = new Library();
+// Make stdlib functions available to be used in JS()
+window.svg = library.svg();
 
 HTMLWidgets.widget({
 
